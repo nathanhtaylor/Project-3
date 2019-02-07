5c9445ae1f947d239ab518e2b69993f0147b8921
nathanhtaylor <nathanhtaylor@tamu.edu>
2019-02-07 17:06:34 -0600

    log_pull()

--- p3.py
+++ p3.py
@@ -2,9 +2,14 @@
 # Nathan Taylor
 #
 # A script to collect and parse log information
+import urllib.request
 
 def log_pull():
-    return
+    log = urllib.request.urlopen('https://s3.amazonaws.com/tcmg476/http_access_log')
+    content = log.read()
+    lines = content.decode()
+    log.close()
+    return lines
 
 def write():
     return
