9c267b69a434dc5ba321a831306d6ac9bcda5255
nathanhtaylor <nathanhtaylor@tamu.edu>
2019-02-07 17:17:39 -0600

    log_write() and AWS_Log.txt creation

--- p3.py
+++ p3.py
@@ -2,17 +2,29 @@
 # Nathan Taylor
 #
 # A script to collect and parse log information
+
 import urllib.request
 
-def log_pull():
-    log = urllib.request.urlopen('https://s3.amazonaws.com/tcmg476/http_access_log')
+def log_pull(inputurl):
+    #print('log_pull check')
+    log = urllib.request.urlopen(inputurl)
     content = log.read()
     lines = content.decode()
+    print('lines: ',lines)
     log.close()
     return lines
 
-def write():
+def log_write(lines):
+    #print('log_write check')
+    file = open('AWS_Log.txt', 'w+')
+    file.write(lines)
+    file.close()
     return
 
 def main():
+    #print('main check')
+    lines = log_pull('https://s3.amazonaws.com/tcmg476/http_access_log')
+    log_write(lines)
     return
+
+main()
